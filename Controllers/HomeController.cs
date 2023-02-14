6ad3c14432511c6cb93fc798f2c6734606788c15
Junyoung Kim <jkim5929@gmail.com>
2023-02-13 19:49:13 -0500

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Deck_of_Cards_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Deck_of_Cards_API.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Deck()
+        {
+            return View(DeckDAL.GetDeck());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/DeckDAL.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Text.Json.Serialization;
+
+namespace Deck_of_Cards_API.Models
+{
+    public class DeckDAL
+    {
+        public static DeckModel GetDeck()
+        {
+
+
+            //setup
+            string url = "https://deckofcardsapi.com/api/deck/new/draw/?count=5";
+
+
+
+
+            //request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();   //casting
+
+            
+            //convert to json
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+           
+            //convert to c#
+            DeckModel result = JsonConvert.DeserializeObject<DeckModel>(JSON);
+
+            return result;
+
+
+        }
+
+
+    }
+}
